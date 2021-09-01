95a93fcc25488387266e41274ede3deb81193347
Михаил Левин <mikelevin@yandex-team.ru>
2021-09-01 22:19:53 +0300

    Lesson 6 ORM

--- src/test/java/BaseApiTest.java
+++ src/test/java/BaseApiTest.java
@@ -1,5 +1,11 @@
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Assertions;
 import utils.PropertyScanner;
 
 abstract class BaseApiTest {
@@ -30,5 +36,26 @@ abstract class BaseApiTest {
 
     public PropertyScanner getScanner() {
         return scanner;
+    }
+
+    public void assertObjects(Object expected, Object actually) throws Exception {
+
+        Field[] fields = expected.getClass().getDeclaredFields();
+
+        Map<String, Field> actuallyFieldsMap = Stream.of(actually.getClass().getDeclaredFields())
+                .collect(Collectors.toMap(
+                        Field::getName,
+                        Function.identity()
+                ));
+
+        for (Field field : fields) {
+            Object expectedValue = field.get(expected);
+            Object actuallyValue = actuallyFieldsMap.get(field.getName()).get(actually);
+            if (!(expectedValue instanceof Number) && !(expectedValue instanceof String)) {
+                assertObjects(expectedValue, actuallyValue);
+            }
+            Assertions.assertEquals(expectedValue, actuallyValue);
+        }
+
     }
 }
